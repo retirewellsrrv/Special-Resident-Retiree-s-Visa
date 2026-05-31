f926ae7fee66f31a2e85ccf557863d7b4fa8f118
Puffssss1 <budji@email.com>
2026-05-31 15:25:47 +0800

    merge: fixed merge conflict and updated the ci

--- src/app/(protected)/admin/layout.tsx
+++ src/app/(protected)/admin/layout.tsx
@@ -1,3 +1,12 @@
+<<<<<<< HEAD
 export default function AdminLayout({ children }: { children: React.ReactNode }) {
   return <>{children}</>
+=======
+export default function AdminLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return <>{children}</>;
+>>>>>>> 187cdab (merge: fixed merge conflict and updated the ci)
 }
