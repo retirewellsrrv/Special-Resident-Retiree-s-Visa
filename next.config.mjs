bfb544b6607b640cac402d4ad73dfecd6c19d493
Elivince <elivince.timbas11@gmail.com>
2026-06-05 15:30:30 +0800

    bugfix(faqs): fix the categories button and accordion

--- next.config.mjs
+++ next.config.mjs
@@ -5,7 +5,7 @@ import { fileURLToPath } from "url";
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const nextConfig = {
-    allowedDevOrigins: ['http://localhost:3000', 'https://srrv.vercel.app', '192.168.254.103'],
+    allowedDevOrigins: ['http://localhost:3000', 'https://srrv.vercel.app', '192.168.254.103', '10.229.139.153'],
     turbopack: {
         root: __dirname, // makes the project folder as root
     },
